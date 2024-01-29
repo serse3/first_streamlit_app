aa23f2faa9428ec14a53413fdec269bb93ea76db
serse3 <90264870+serse3@users.noreply.github.com>
2024-01-29 17:11:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.header('Breakfast Menu')
 
 # Read the fruit list from a CSV file
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 
 # Display the DataFrame
 streamlit.dataframe(my_fruit_list)
