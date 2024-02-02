cc846688eeda458e4badf6562e82a3ebf7a6f55c
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-02 13:16:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit
 import pandas
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.header('Breakfast Menu')
 # Read the fruit list from a CSV file
@@ -13,4 +10,14 @@ fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the DataFrame
 streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
 
