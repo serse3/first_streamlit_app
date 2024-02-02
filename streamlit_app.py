267f69a3a22b776e51699853d51c0ef7cbee4be5
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-02 13:13:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.header('Breakfast Menu')
 # Read the fruit list from a CSV file
