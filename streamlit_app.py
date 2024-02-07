a95a50944f70421834acc503ee0e569cde59b811
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-07 09:17:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.header("The fruit load list contains:")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 streamlit.header('Breakfast Menu')
