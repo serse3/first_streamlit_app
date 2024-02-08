71d55546e0c6744c87038010ba13dc71006e26ad
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-08 16:10:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,11 @@ def Top():
   streamlit.dataframe(fruits_to_show)
 def getFruit():
   try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
       streamlit.error("Please select a furit to get information.")
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{fruit_choice}")
       # write your own comment -what does the next line do? 
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
