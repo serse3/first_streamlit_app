6202b301508000a8582d47e626885818c2d31874
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-08 15:46:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,31 +4,39 @@ import snowflake.connector
 import requests
 from urllib.error import URLError
 
-#Otros elementos visuales: 
-streamlit.header('Breakfast Menu')
-# Read the fruit list from a CSV file
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
-# Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the DataFrame
-streamlit.dataframe(fruits_to_show)
+
+  #Otros elementos visuales: 
+  streamlit.header('Breakfast Menu')
+def Top():    
+
+  # Read the fruit list from a CSV file
+  my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+  # Display the table on the page.
+  my_fruit_list = my_fruit_list.set_index('Fruit')
+  fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+  fruits_to_show = my_fruit_list.loc[fruits_selected]
+  # Display the DataFrame
+  streamlit.dataframe(fruits_to_show)
 
 
+Top()
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-  if not fruit_choice:
-    streamlit.error("Please select a furit to get information.")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+getFruit()
+
+
+def getFruit():
+  try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+      streamlit.error("Please select a furit to get information.")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
+  except URLError as e:
+    streamlit.error()
 
 
 
