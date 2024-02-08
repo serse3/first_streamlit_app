4753081c7bf92d8b601c78a17730cda27821f395
serse3 <90264870+serse3@users.noreply.github.com>
2024-02-08 16:12:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def getFruit():
     if not fruit_choice:
       streamlit.error("Please select a furit to get information.")
     else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{fruit_choice}")
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       # write your own comment -what does the next line do? 
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # write your own comment - what does this do?
